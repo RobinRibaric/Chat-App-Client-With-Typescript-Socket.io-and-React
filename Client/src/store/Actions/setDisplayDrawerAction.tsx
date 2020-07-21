ff48d526c331646e66a65114211aa396024448a4
RobinRibaric <zakariasribaric@hotmail.com>
2020-07-21 14:05:25 +0200

    added css custom properties for colors

--- Client/src/store/Actions/setDisplayDrawerAction.tsx
+++
@@ -1,9 +0,0 @@
-import {
-    ActionTypes,
-    SET_DISPLAYDRAWER,
-  } from '../types';
-
-export const setDisplayDrawerAction = (displayDrawer: boolean): ActionTypes => ({
-    type: SET_DISPLAYDRAWER,
-    payload: displayDrawer,
-  });
--- Client/src/store/index.ts
+++ Client/src/store/index.ts
@@ -3,13 +3,10 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import ErrorReducer from './Reducers/ErrorReducer';
 import UsersReducer from './Reducers/UsersReducer';
-import DisplayDrawerReducer from './Reducers/DisplayDraweReducer';
-
 
 const rootReducer = combineReducers({
     ErrorReducer,
     UsersReducer,
-    DisplayDrawerReducer,
   });
 
   export const store = createStore(rootReducer, composeWithDevTools());
