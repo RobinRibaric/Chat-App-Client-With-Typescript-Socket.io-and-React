9068d174fb6225c551a47237c010647b74535e06
RobinRibaric <zakariasribaric@hotmail.com>
2020-07-22 11:52:20 +0200

    new endpoint

--- Client/src/pages/ChatPage/Chat.tsx
+++ Client/src/pages/ChatPage/Chat.tsx
@@ -41,7 +41,7 @@ const Chat: React.FC<Props> = ({ location }) => {
     const [SideDrawerOpen, setSideDrawerOpen] = useState<boolean>(false);
 
     const dispatch = useDispatch();
-    const ENDPOINT = 'https://react-chatt-aplication.herokuapp.com/';
+    const ENDPOINT = 'https://chatt-app-server.herokuapp.com/';
 
     useEffect(() => {
         const { name } = queryString.parse(location.search);
